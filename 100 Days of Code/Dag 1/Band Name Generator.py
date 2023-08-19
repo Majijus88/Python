c67348a0238de9969dbdef1836ff4d0f11b74682
Majijus88 <marius.holtmoen@gmail.com>
2023-08-19 22:17:28 +0200

    Første dag av 100 Days of Code

---
+++ 100 Days of Code/Dag 1/Band Name Generator.py
@@ -0,0 +1,5 @@
+#Gir bruker prompt om å skrive inn navnet på byen hen ble født
+By = input("Hvilken by ble du født? ")
+Dyr = input("Hva het ditt første dyr? ")
+Kombinert = (f"{By} {Dyr}")
+print(f"Basert på inputen din foreslås det at du navngir bandet ditt: {Kombinert}!")
---
+++ 100 Days of Code/Dag 1/Lengde på string.py
@@ -0,0 +1,8 @@
+#len() gir mulighet til å telle antallet tegn som utgjør en string
+
+min_streng = "Heisann hoppsann!"
+
+#For å finne ut hvor mange tegn som utgjør innholdet i variabelen "min_streng" benytter jeg len() funksjonen under
+#I dette tilfellet gir det 17 tegn
+print(len(min_streng))
+
