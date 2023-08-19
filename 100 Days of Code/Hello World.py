c2ed74179ce2e0c1278ef206a699bdec6f96d140
Majijus88 <marius.holtmoen@gmail.com>
2023-08-19 13:26:38 +0200

    Første commit

---
+++ 100 Days of Code/Hello World.py
@@ -0,0 +1,10 @@
+#Dette er en test for å sjekke at koden funker#
+
+Kode = "hello World"
+print(f"{Kode} er en av de vanligste måtene å teste sin første kode")
+
+Stor = Kode.capitalize()
+print(f"{Stor} er en av de vanligste måtene å teste sin første kode")
+
+Liten = Kode.lower()
+print(f"{Liten} er en av de vanligste måtene å teste sin første kode")
